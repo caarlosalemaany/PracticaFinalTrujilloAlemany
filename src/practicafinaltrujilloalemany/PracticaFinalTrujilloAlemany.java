7020dd4487ab821d44328e99420274b78fd11e5c
caarlosalemaany <151624715+caarlosalemaany@users.noreply.github.com>
2023-12-19 10:28:15 +0100

    Update PracticaFinalTrujilloAlemany.java
    cambia merica

--- src/practicafinaltrujilloalemany/PracticaFinalTrujilloAlemany.java
+++ src/practicafinaltrujilloalemany/PracticaFinalTrujilloAlemany.java
@@ -16,6 +16,7 @@ public class PracticaFinalTrujilloAlemany {
     public static void main(String[] args) {
         // TODO code application logic here
         //cambio desde github
+        // cambio desde github 2
     }
     
 }
